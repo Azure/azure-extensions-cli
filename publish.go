a8ef6ad0b805b33b9d892537b1f57358090e1d9b
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2016-01-27 10:17:19 -0800

    factor out uploadBlob

--- publish.go
+++ publish.go
@@ -20,47 +20,15 @@ const (
 func createExtension(c *cli.Context) {
 	cl := mkClient(checkFlag(c, flSubsID.Name), checkFlag(c, flSubsCert.Name))
 
+	// Upload blob
+	blobURL := uploadBlob(cl, checkFlag(c, flStorageAccount.Name), checkFlag(c, flPackage.Name))
+	log.Debugf("Extension package uploaded to: %s", blobURL)
+
 	// Read manifest
 	b, err := ioutil.ReadFile(checkFlag(c, flManifest.Name))
 	if err != nil {
 		log.Fatalf("Error reading manifest: %v", err)
 	}
-
-	// Fetch keys for storage account
-	stg := checkFlag(c, flStorageAccount.Name)
-	svc := storageservice.NewClient(cl.client)
-	keys, err := svc.GetStorageServiceKeys(stg)
-	if err != nil {
-		log.WithField("name", "stg").Fatalf("Could not fetch keys for storage account. Make sure it is in publisher subscription. Error: %v", err)
-	}
-	log.Debug("Retrieved storage account keys.")
-
-	// Read package
-	pkg, err := os.OpenFile(checkFlag(c, flPackage.Name), os.O_RDONLY, 0777)
-	if err != nil {
-		log.Fatalf("Could not reach package file: %v", err)
-	}
-	stat, err := pkg.Stat()
-	if err != nil {
-		log.Fatalf("Could not stat the package file: %v", err)
-	}
-	defer pkg.Close()
-
-	// Upload blob
-	sc, err := storage.NewBasicClient(stg, keys.PrimaryKey)
-	if err != nil {
-		log.Fatalf("Could not create storage client: %v", err)
-	}
-	bs := sc.GetBlobService()
-	if _, err := bs.CreateContainerIfNotExists(containerName, storage.ContainerAccessTypeBlob); err != nil {
-		log.Fatalf("Error creating blob container: %v", err)
-	}
-	blobName := fmt.Sprintf("%d.zip", time.Now().Unix())
-	if err := bs.CreateBlockBlobFromReader(containerName, blobName, uint64(stat.Size()), pkg, nil); err != nil {
-		log.Fatalf("Error uploading blob: %v", err)
-	}
-	blobURL := bs.GetBlobURL(containerName, blobName)
-	log.Debugf("Extension package uploaded to: %s", blobURL)
 
 	// Replace %BLOB_URL% in the manifest.
 	manifest := string(b)
@@ -77,4 +45,43 @@ func createExtension(c *cli.Context) {
 	}
 	log.Info("CreateExtension operation finished.")
 	log.Info("Next steps: Test in your subscription, promote to pilot regions and then PROD.")
+}
+
+func updateExtension(c *cli.Context) {
+}
+
+func uploadBlob(cl ExtensionsClient, storageAccount, packagePath string) string {
+	// Fetch keys for storage account
+	svc := storageservice.NewClient(cl.client)
+	keys, err := svc.GetStorageServiceKeys(storageAccount)
+	if err != nil {
+		log.WithField("name", "storageAccount").Fatalf("Could not fetch keys for storage account. Make sure it is in publisher subscription. Error: %v", err)
+	}
+	log.Debug("Retrieved storage account keys.")
+
+	// Read package
+	pkg, err := os.OpenFile(packagePath, os.O_RDONLY, 0777)
+	if err != nil {
+		log.Fatalf("Could not reach package file: %v", err)
+	}
+	stat, err := pkg.Stat()
+	if err != nil {
+		log.Fatalf("Could not stat the package file: %v", err)
+	}
+	defer pkg.Close()
+
+	// Upload blob
+	sc, err := storage.NewBasicClient(storageAccount, keys.PrimaryKey)
+	if err != nil {
+		log.Fatalf("Could not create storage client: %v", err)
+	}
+	bs := sc.GetBlobService()
+	if _, err := bs.CreateContainerIfNotExists(containerName, storage.ContainerAccessTypeBlob); err != nil {
+		log.Fatalf("Error creating blob container: %v", err)
+	}
+	blobName := fmt.Sprintf("%d.zip", time.Now().Unix())
+	if err := bs.CreateBlockBlobFromReader(containerName, blobName, uint64(stat.Size()), pkg, nil); err != nil {
+		log.Fatalf("Error uploading blob: %v", err)
+	}
+	return bs.GetBlobURL(containerName, blobName)
 }
